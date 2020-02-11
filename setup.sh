d159c3c59fe8ffcce7fd428a12673e5cc791cf40
太田原香菜 <kana@ootawarakousainoMacBook-Pro.local>
2020-02-11 16:11:26 +0900

    zprezto周りの設定

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.tmux.conf .vim .vimrc .vimrc.keymap .zshrc .zpreztorc)
+DOT_FILES=(.tmux.conf .vim .vimrc .vimrc.keymap .zlogin .zlogout .zprezto .zpreztorc .zprofile .zshenv .zshrc)
 
 for file in ${DOT_FILES[@]}
 do
