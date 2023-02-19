1b7d7a1f06cdd0a6217a25db42b5c5fe7f89c844
Kana Otawara <shimmer22.0204@gmail.com>
2023-02-19 19:41:50 +0900

    不要なfunction削除

--- .zsh.d/functions.zsh
+++ .zsh.d/functions.zsh
@@ -8,37 +8,6 @@ function mkcd() {
   fi
 }
 
-
-#
-# Goolge Search by Google Chrome
-# terminalからググったりqiita検索をできる
-#
-google() {
-    local str opt
-    if [ $# != 0 ]; then
-        for i in $*; do
-            # $strが空じゃない場合、検索ワードを+記号でつなぐ(and検索)
-            str="$str${str:++}$i"
-        done
-        opt='search?num=100'
-        opt="${opt}&q=${str}"
-    fi
-    open -a Google\ Chrome http://www.google.co.jp/$opt
-}
-
-qiita() {
-    local str opt
-    if [ $# != 0 ]; then
-        for i in $*; do
-            # $strが空じゃない場合、検索ワードを+記号でつなぐ(and検索)
-            str="$str${str:++}$i"
-        done
-        opt='search?num=100'
-        opt="${opt}&q=${str}"
-    fi
-    open -a Google\ Chrome http://qiita.com/$opt
-}
-
 select-history() {
   # コマンドの実行履歴をfzfで選択し、コマンドラインへ書き込む
   BUFFER=$(history -n -r 1 | fzf --query "$BUFFER")
